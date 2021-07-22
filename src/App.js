bbb5459fc194d7fe3177d5682be2f7539004a7ba
Dporres20 <dporres20@gmail.com>
2021-07-21 23:13:58 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import './App.css';
 import { ChakraProvider } from "@chakra-ui/react"
 function App() {
 
-  const lat = 42.7070;
-  const long = 71.1631;
+  const lat = 42.346268;
+  const long = -71.095764;
   const gate = "https://cbi-store-api-qa.herokuapp.com/v3/stores/?apiKey=compass&signature=0QJkbTzuM/WSxz9oGuUnPR4uJtGjZ4PHEUfgNJ56L/0=&beverageType=beer&latitude=" + lat + "&longitude=" + long + "&radius=5&searchText=Safe%2A"
 // https://cbi-store-api-qa.herokuapp.com/v3/stores/?apiKey=compass&signature=0QJkbTzuM/WSxz9oGuUnPR4uJtGjZ4PHEUfgNJ56L/0=&beverageType=beer&latitude=37.33233141&longitude=-122.0312186&radius=5&searchText=Safe%2A
   const getInfo = () => {
