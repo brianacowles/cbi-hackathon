813995ce106479c3c6ddd1758f2d59205700e652
Dporres20 <dporres20@gmail.com>
2021-07-22 14:38:06 -0400

    Created Components
    Added 3 components to the file. One hold the code to create List Items to be populated by infomration passed in from an API call
    
    Issues: Trouble getting the information populated as needed.

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
 import React from 'react';
 import axios from 'axios';
 import './App.css';
-import { ChakraProvider } from "@chakra-ui/react"
+import CreateItems from "./components/CreateItems"
+import Items from "./components/Items"
+
+
 function App() {
 
   const lat = 42.346268;
   const long = -71.095764;
-  const gate = "https://cbi-store-api-qa.herokuapp.com/v3/stores/?apiKey=compass&signature=0QJkbTzuM/WSxz9oGuUnPR4uJtGjZ4PHEUfgNJ56L/0=&beverageType=beer&latitude=" + lat + "&longitude=" + long + "&radius=5&searchText=Safe%2A"
-// https://cbi-store-api-qa.herokuapp.com/v3/stores/?apiKey=compass&signature=0QJkbTzuM/WSxz9oGuUnPR4uJtGjZ4PHEUfgNJ56L/0=&beverageType=beer&latitude=37.33233141&longitude=-122.0312186&radius=5&searchText=Safe%2A
-  const getInfo = () => {
+  const rad = 50;
+  const gate = "https://cbi-store-api-qa.herokuapp.com/v3/stores/?apiKey=compass&signature=0QJkbTzuM/WSxz9oGuUnPR4uJtGjZ4PHEUfgNJ56L/0=&beverageType=beer&latitude=" + lat + "&longitude=" + long + "&radius="+ rad +"&searchText=Safe%2A";
+   const inf = [];
+
+
+// API REQUEST Strings
+/* https://cbi-store-api-qa.herokuapp.com/v3/stores/?apiKey=compass&signature=0QJkbTzuM/WSxz9oGuUnPR4uJtGjZ4PHEUfgNJ56L/0=&beverageType=beer&latitude=37.33233141&longitude=-122.0312186&radius=5&searchText=Safe%2A
+  https://cbi-store-api-qa.herokuapp.com/v3/stores/?apiKey=compass&signature=0QJkbTzuM/WSxz9oGuUnPR4uJtGjZ4PHEUfgNJ56L/0=&beverageType=beer&latitude=42.346268&longitude=-71.095764&radius=50&searchText=Safe%2A
+  "https://cbi-store-api-qa.herokuapp.com/v3/stores/?apiKey=compass&signature=0QJkbTzuM/WSxz9oGuUnPR4uJtGjZ4PHEUfgNJ56L/0=&beverageType=beer&latitude=42.346268&longitude=-71.095764&radius=1" */
+
+
+  const GetInfo = () => {
     axios.get(gate).then((response)=> {
-      console.log(response);
+      
+      // Response.data is an array
+      {/* Loop through the response and create a li element called item */}
+      {response.data.map(storeInfo => (<Items key={Date.now()} storeName={storeInfo.storeName} storeType={storeInfo.storeType} />))}
+
+      
+      // console.log(response.data[0].storeName);
+      // console.log(response.data);
     })
   }
-
-  
-  const getJoke = () => {
-    axios.get("https://official-joke-api.appspot.com/random_joke").then((response)=>{
-      console.log(response);
-    })
-  }
-
   return (
-    <ChakraProvider>
-        <div className="App">
 
-        <button onClick={getJoke}>Get Joke Right now</button>
+    <div className="App">
+      <button onClick={GetInfo}>Get your CBI Information</button>
+      <ul>
+        <GetInfo />
+      </ul>
+      
 
-        <br></br>
-
-        <button onClick={getInfo}>Get your CBI Information</button>
-
-        </div>
-    </ChakraProvider>
+    </div>
+    
   );
 }
 
---
+++ src/components/CreateItems.js
@@ -0,0 +1,47 @@
+//My function for the page
+const CreateBox = ({storeInfo}) =>{
+    return(
+        // Contains main content of the button
+        <ul id="Button-Content">
+            {/* Making the list items */}
+            <li id="items">
+                {/* Title Area */}
+                <div id="Title-Area">
+                    {/* Title for the button will be gotten from the API call that we get */}
+                    <h1 id="Main-Title">{storeInfo.title}</h1>
+
+                    {/* This will hold the distance form the current location to the suggested location with siaplyed info */}
+                    <div id="subtitle-info">
+                        <div className="info">5M</div>
+                        <div className="info"> storeInfo.type </div>
+                    </div>
+                </div>
+
+                {/* This area will contain the description and the share features */}
+                <div id="description-area">
+                    {/* This area will contain the Information about the locaiton */}
+                    <div id="Locaiton-Description">
+                        <div>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod lacinia blandit. Nulla vestibulum quis dolor efficitur semper. Suspendisse sed cursus nisl. Quisque ipsum ligula, efficitur in porttitor congue, sodales vel velit. Morbi quis mi sit amet augue semper auctor id euismod nisl. Proin finibus orci quis magna pulvinar interdum.</div>
+                    </div>
+                    <ul className="btns-container">
+                        <li className="btn-item">
+                            <a href="#" className="btn-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{fill: "black"}}><circle cx="17" cy="4" r="2"></circle><path d="M15.777 10.969a2.007 2.007 0 0 0 2.148.83l3.316-.829-.483-1.94-3.316.829-1.379-2.067a2.01 2.01 0 0 0-1.272-.854l-3.846-.77a1.998 1.998 0 0 0-2.181 1.067l-1.658 3.316 1.789.895 1.658-3.317 1.967.394L7.434 17H3v2h4.434c.698 0 1.355-.372 1.715-.971l1.918-3.196 5.169 1.034 1.816 5.449 1.896-.633-1.815-5.448a2.007 2.007 0 0 0-1.506-1.33l-3.039-.607 1.772-2.954.417.625z" className="fa-primary"></path></svg>
+                            </a>
+                            <span className="link-text">Directions</span>
+                        </li>
+                        <li className="btn-item">
+                            <a href="#" className="btn-link">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style={{fill: "black"}}><path d="M11 15h2V9h3l-4-5-4 5h3z"></path><path d="M20 18H4v-7H2v7c0 1.103.897 2 2 2h16c1.103 0 2-.897 2-2v-7h-2v7z"></path></svg>
+                            </a>
+                            <span className="link-text">Share</span>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+        </ul>
+        
+    )
+}
+
+export default CreateBox
