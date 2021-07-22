8244504e7023223c2c4e6d302f5fc57c417bb093
Dporres20 <dporres20@gmail.com>
2021-07-21 22:52:21 -0400

    Started Adding the API cal
    Added the initial API, working on getting lat and long param added to query so we can use different Lats and Longs

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
+import React from 'react';
+import axios from 'axios';
 import './App.css';
 import { ChakraProvider } from "@chakra-ui/react"
 function App() {
+
+  const lat = 42.7070;
+  const long = 71.1631;
+  const gate = "https://cbi-store-api-qa.herokuapp.com/v3/stores/?apiKey=compass&signature=0QJkbTzuM/WSxz9oGuUnPR4uJtGjZ4PHEUfgNJ56L/0=&beverageType=beer&latitude=" + lat + "&longitude=" + long + "&radius=5&searchText=Safe%2A"
+// https://cbi-store-api-qa.herokuapp.com/v3/stores/?apiKey=compass&signature=0QJkbTzuM/WSxz9oGuUnPR4uJtGjZ4PHEUfgNJ56L/0=&beverageType=beer&latitude=37.33233141&longitude=-122.0312186&radius=5&searchText=Safe%2A
+  const getInfo = () => {
+    axios.get(gate).then((response)=> {
+      console.log(response);
+    })
+  }
+
+  const getJoke = () => {
+    axios.get("https://official-joke-api.appspot.com/random_joke").then((response)=>{
+      console.log(response);
+    })
+  }
+
   return (
     <ChakraProvider>
         <div className="App">
+
+        <button onClick={getJoke}>Get Joke Right now</button>
+
+        <br></br>
+
+        <button onClick={getInfo}>Get your CBI Information</button>
+
         </div>
     </ChakraProvider>
   );
